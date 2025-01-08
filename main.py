69224196f818031c8fccf8c2fc5a2665e01d96ae
Ronosavio <ronosavio7@gmail.com>
2025-01-08 13:26:31 +0530

    using some packages in fastapi

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app  = FastAPI()
 
@@ -6,3 +7,15 @@ app  = FastAPI()
 @app.get("/")
 def root():
    return{"message : Welcome to my api!!"}
+
+@app.get("/login") 
+def login():
+    return {"You have entered the login page"}
+ 
+@app.get("/post")
+def posts():
+   return {"These are the overall post"}
+
+@app.post("/createpost")
+def create_post(payload: dict = Body(...)):
+    return {"new_post":f"title {payload['title']} content:{payload['content']}"}
