0953fc5e9f55ddba3af03b44b727237b6f1b8ee0
Ronosavio <ronosavio7@gmail.com>
2025-03-12 12:33:33 +0530

    some more progress using pydantic

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
 
-app  = FastAPI()
+
+app = FastAPI()
+
+class Post(BaseModel):
+   title: str
+   content: str
+   published : bool = True 
+   rating: Optional[int] = None
 
 #path operation
 @app.get("/")
@@ -17,5 +26,11 @@ def posts():
    return {"These are the overall post"}
 
 @app.post("/createpost")
-def create_post(payload: dict = Body(...)):
-    return {"new_post":f"title {payload['title']} content:{payload['content']}"}
+def create_post(new_post: Post):
+    print(new_post.dict())
+    return {"data":"new_post"}
+ 
+ #title str, content str, 
+ 
+
+   
